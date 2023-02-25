e7ad62da19c7becdd47dcbea4816e6474ddda24b
navidkianfar <t5bushVeJn2TDaFYmWZ7Xseb6XcQjqfkmFb5uFeUJF3wxUmJbgcLTXbVm3Zj83Lp>
2023-02-25 22:52:48 +0300

    angular configuration done

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api_endpoint: 'https://drethos.microethos.com/api/v1/drethos',
+  lang: 'en',
+  culture: 'en-US',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  api_endpoint: '/proxy-api',
+  lang: 'en',
+  culture: 'en-US',
+  // socketEndpoint: 'http://localhost:3000',
+  socketEndpoint: 'https://socket-panel-omni.microethos.com/',
+};
